4261fea5ed75fa76c8d85e1fe55624f3a86809d3
Andres Grisales <andres.grisalesgg@gmail.com>
2021-01-18 08:10:30 -0500

    Fix bugs x2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/eplatform-angular'));
+app.use(express.static(__dirname + '/dist/argon-design-system-angular'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/eplatform-angular/index.html'));
+        '/dist/argon-design-system-angular/index.html'));
 });
 app.listen(process.env.PORT || 8080);
